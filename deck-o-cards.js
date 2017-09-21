7e4ccedff6ba094da3cf94c8c21f1d6a1e01b419
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-20 23:46:46 -0500

    created deck logic

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,49 +1,68 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
-// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
+
 function deck_o_cards() {
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
+  var pokerHand = []; // poker hand
+  var objIndex = 0; //card counter
+  var pokerStatement = "Your poker hand is a(n)"; //string to tell you your poker hand
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
+  // Makes 52 card objects and store them in the "cards" array
+  for (i=0; i < suits.length; i++){
+    var currentSuit = suits[i];
+    for (p=0; p < values.length; p++){
+      var currentValue = values[p];
+      var objToAdd = {suit: currentSuit, value: currentValue};
+      cards[objIndex] = objToAdd;
+      objIndex += 1;
+    };
+  };
 
+  // Shuffles the cards
+  function shuffle(array) {
+      var counter = array.length, temp, index;
+      // While there are elements in the array
+      while (counter > 0) {
+          // Pick a random index
+          index = Math.floor(Math.random() * counter);
+          // Decrease counter by 1
+          counter--;
+          // And swap the last element with it
+          temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+      }
+      return array;
+  };
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+  shuffledCards = shuffle(cards);
 
+  //Makes poker hand
+  for (i=0; i < 5; i++){
+    pokerHand[i] = shuffledCards[i];
+  };
+
+  for(i=0; i < pokerHand.length; i++){
+    if (i == pokerHand.length-1){
+      pokerStatement += "and a(n) " + pokerHand[i].value + " of " + pokerHand[i].suit + ". ";
+      break;
+    }
+    pokerStatement += "a(n) " + pokerHand[i].value + " of " + pokerHand[i].suit + ", ";
+  }
 
   // Pull the top card from the newly shuffledCards
 
+  var topCard = shuffledCards[1];
 
   // 3. Print the results:
   // "The deck has {n} cards"
   // "The top card is the {value} of {suit}"
 
-}
+  console.log("The deck has " + objIndex + " cards");
+  console.log("The top card is the " + topCard.value + " of " + topCard.suit);
+  console.log();
+  console.log(pokerStatement);
 
-
-
-// Fisher-Yates Shuffle
-// http://stackoverflow.com/a/6274398
-function shuffle(array) {
-    var counter = array.length, temp, index;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
-}
+};
